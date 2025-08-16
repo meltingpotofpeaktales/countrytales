020dbdb08cc5679a57e426ceccb42a3f8afc90ae
meltingpotofpeaktales <jaydend262012@gmail.com>
2025-08-15 18:44:11 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-function gameStartSound () {
+function play_the_intro () {
     music.play(music.createSoundEffect(WaveShape.Noise, 1813, 1110, 255, 255, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.createSoundEffect(WaveShape.Noise, 1110, 1374, 255, 255, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.createSoundEffect(WaveShape.Noise, 1813, 5000, 255, 255, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.createSoundEffect(WaveShape.Noise, 5000, 2120, 255, 255, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 }
-gameStartSound()
+play_the_intro()
+pause(100)
